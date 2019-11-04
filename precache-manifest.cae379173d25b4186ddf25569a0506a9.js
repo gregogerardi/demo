81a8d4c37ac850e2aca89cb5ab8cdfa285f4d278
Gregorio Gerardi <Gregorio.Gerardi@ibm.com>
2019-11-04 19:59:31 -0300

    Updates

---
+++ precache-manifest.cae379173d25b4186ddf25569a0506a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846230257b6341e9daa36d27dd87d1be",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "b2345664f9deb5d982c1",
+    "url": "/demo/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "2023a8d0cf47eb8d077a",
+    "url": "/demo/static/js/2.3d98282c.chunk.js"
+  },
+  {
+    "revision": "b2345664f9deb5d982c1",
+    "url": "/demo/static/js/main.c75edef6.chunk.js"
+  },
+  {
+    "revision": "8e5c9404311416d6830f",
+    "url": "/demo/static/js/runtime-main.137b1daf.js"
+  },
+  {
+    "revision": "11b179c43d2ca794f5423c840087ba4f",
+    "url": "/demo/static/media/logo.11b179c4.svg"
+  },
+  {
+    "revision": "bb68dcbcf7832e4ebca07493bdf765b4",
+    "url": "/demo/static/media/spotify.bb68dcbc.svg"
+  }
+]);
